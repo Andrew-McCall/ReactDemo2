c085ae5914fda28011b4e2fb8defc862aadd3882
Andrew-McCall <amccall@qa.com>
2022-10-11 09:19:09 +0100

    Search bar

--- src/components/stateExample.jsx
+++ src/components/stateExample.jsx
@@ -3,11 +3,13 @@ import {useState} from "react"
 const Clicker = () => {
 
     const [count, setCount] = useState(0);
+    const [search, setSearch] = useState("")
 
     const buttonPress = (e) => {
         e.preventDefault();
 
         setCount(count+1);
+        setSearch(count+1)
         console.log(count)
     }
 
@@ -15,6 +17,10 @@ const Clicker = () => {
 
         <button onClick={buttonPress}> Click Me! </button>
         <p>Count: {count} </p>
+        <br/>
+
+        <input type="text" value={search} onChange={(e) => {e.preventDefault(); setSearch(e.target.value)}}></input>
+        <p> Serach: {search} </p>
 
     </div>)
 
